bac0be9a6576b9d08b62753038487d453e304dd7
tony76179360 <142843162+tony76179360@users.noreply.github.com>
2024-10-27 02:01:52 -0500

    commit 3

--- src/licenciasmunicipales/LicenciasMunicipales.java
+++ src/licenciasmunicipales/LicenciasMunicipales.java
@@ -16,6 +16,7 @@ public class LicenciasMunicipales {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("ejemplo");
+        System.out.println("prueba commit");
         // TODO code application logic here
     }
     
